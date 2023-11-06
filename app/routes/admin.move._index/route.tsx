c6a0fdec2918fbca91fc5dc0ccaebc3aff6c9a68
sfujimotosfujimoto <unicoshun@gmail.com>
2023-11-07 07:19:17 +0900

    maxDuration60

--- app/routes/admin.move._index/route.tsx
+++ app/routes/admin.move._index/route.tsx
@@ -30,7 +30,8 @@ import { useToast } from "~/hooks/useToast"
 import { authenticate } from "~/lib/authenticate.server"
 
 export const config = {
-  maxDuration: 15,
+  // TODO: set maxDuration for production
+  maxDuration: 60,
 }
 
 /**
