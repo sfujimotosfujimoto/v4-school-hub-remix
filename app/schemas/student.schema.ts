038e38badce47d275aa893612425c77ce62916af
sfujimotosfujimoto <unicoshun@gmail.com>
2024-01-05 19:34:31 +0900

    fix typ

--- app/schemas/student.schema.ts
+++ app/schemas/student.schema.ts
@@ -10,5 +10,5 @@ export const StudentSchema = z.object({
   sei: z.string(),
   mei: z.string(),
   email: z.string(),
-  folerLink: z.string().optional().nullable(),
+  folderLink: z.string().optional().nullable(),
 })
