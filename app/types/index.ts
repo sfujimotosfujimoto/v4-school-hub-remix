c82accaf64b648bac0d5f9a03e529d46225d4519
sfujimotosfujimoto <unicoshun@gmail.com>
2024-01-17 07:39:20 +0900

    update types

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,24 +1,9 @@
-import { Schema } from "zod"
 import type { DriveFile, Student, DriveFileMeta } from "."
-import { drive_v3 } from "googleapis"
+import type { drive_v3 } from "googleapis"
 
 export type * from "./prisma-types"
 export type * from "./google-types"
 export type * from "./zod-types"
-
-// export type {
-//   Credential as PrismaCredential,
-//   // DriveFileData as PrismaDriveFileData,
-//   Stats as PrismaStats,
-//   User as PrismaUser,
-// } from "@prisma/client"
-
-// rexp = refresh token expiry date
-export type Payload = {
-  email: string
-  exp: number
-  rexp: number
-}
 
 export type Gakunen = "ALL" | "J1" | "J2" | "J3" | "H1" | "H2" | "H3"
 export type Hr = "ALL" | "A" | "B" | "C" | "D" | "E" | "F"
@@ -36,68 +21,24 @@ export type Task = {
 export type DriveFileMove = Pick<DriveFile, "id" | "parents"> & {
   meta: Pick<DriveFileMeta, "destination" | "last">
 }
-export type DriveFileRename = Pick<DriveFile, "id"> & {
-  meta: Pick<DriveFileMeta, "file">
-}
 
 export type DriveFileTask = Pick<DriveFile, "id" | "parents"> & {
   meta: DriveFileMeta
 }
 
+// used for transfering data for admin actions
 export type ActionResponse = {
   successFiles: drive_v3.Schema$File[]
   errorFiles: drive_v3.Schema$File[]
 }
-// export type DriveFileData = Omit<
-//   PrismaDriveFileData,
-//   "createdTime" | "modifiedTime" | "firstSeen" | "lastSeen" | "appProperties"
-// > & {
-//   createdTime: number
-//   modifiedTime: number
-//   firstSeen: number
-//   lastSeen: number
-//   appProperties: {
-//     [key: string]: string | null
-//   } | null
+
+// export type DriveFileRename = Pick<DriveFile, "id"> & {
+//   meta: Pick<DriveFileMeta, "file">
 // }
 
-// export interface User {
-//   id: number
-//   first: string
-//   last: string
+// // rexp = refresh token expiry date
+// export type Payload = {
 //   email: string
-//   picture: string
-//   role: Role
-//   activated: boolean
-//   createdAt: Date
-//   updatedAt: Date
-//   credential: Credential | null
-//   stats: Stats | null
-//   student?: Student | null
-//   studentGakuseki?: number | null
-//   driveFileData?: DriveFileData[] | null
-// }
-
-// type Credential = {
-//   accessToken: string
-//   expiry: number
-//   refreshToken: string | null
-//   refreshTokenExpiry: number
-//   createdAt: Date
-// }
-
-// export type Student = {
-//   gakuseki: number
-//   gakunen: string
-//   hr: string
-//   hrNo: number
-//   last: string
-//   first: string
-//   sei: string
-//   mei: string
-//   email: string
-//   folderLink?: string | null
-//   userId?: number
-//   createdAt?: Date
-//   expiry?: number
+//   exp: number
+//   rexp: number
 // }
