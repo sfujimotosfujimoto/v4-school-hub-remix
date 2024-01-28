990c4b2a5af894b0dae4dad9e35169269f9d951f
sfujimotosfujimoto <unicoshun@gmail.com>
2024-01-29 08:05:21 +0900

    logger to pino

--- app/logger/index.ts
+++ app/logger/index.ts
@@ -1,21 +1,33 @@
-import winston from "winston"
+import pino from "pino"
 
-const logFormatter = winston.format.combine(
-  winston.format.timestamp({
-    format: "YY-MM-DD hh:mm:ss",
-  }),
-  winston.format.printf((meta) => {
-    const { level, message, timestamp } = meta
-
-    return `${timestamp} ${level}: ${message}`
-  }),
-  winston.format.colorize({
-    all: true,
-  }),
-  winston.format.align(),
-)
-export const logger = winston.createLogger({
+export const logger = pino({
   level: process.env.LOG_LEVEL || "info",
-  format: logFormatter,
-  transports: [new winston.transports.Console()],
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
 })
+
+// import winston from "winston"
+
+// const logFormatter = winston.format.combine(
+//   winston.format.timestamp({
+//     format: "YY-MM-DD hh:mm:ss",
+//   }),
+//   winston.format.printf((meta) => {
+//     const { level, message, timestamp } = meta
+
+//     return `${timestamp} ${level}: ${message}`
+//   }),
+//   winston.format.colorize({
+//     all: true,
+//   }),
+//   winston.format.align(),
+// )
+// export const logger = winston.createLogger({
+//   level: process.env.LOG_LEVEL || "info",
+//   format: logFormatter,
+//   transports: [new winston.transports.Console()],
+// })
