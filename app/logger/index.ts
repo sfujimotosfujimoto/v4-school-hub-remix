7a831fa5f0ffc07c53b8506b2cc632e84744f799
sfujimotosfujimoto <unicoshun@gmail.com>
2024-02-02 20:52:18 +0900

    update

--- app/logger/index.ts
+++ app/logger/index.ts
@@ -10,35 +10,6 @@ const stream = pretty({
 export const logger = pino(
   {
     level: process.env.NODE_ENV === "development" ? "debug" : "info",
-    // name: "sf-logger",
-    // transport: {
-    //   target: "pino-pretty",
-    //   options: {
-    //     colorize: true,
-    //   },
-    // },
   },
   stream,
 )
-
-// import winston from "winston"
-
-// const logFormatter = winston.format.combine(
-//   winston.format.timestamp({
-//     format: "YY-MM-DD hh:mm:ss",
-//   }),
-//   winston.format.printf((meta) => {
-//     const { level, message, timestamp } = meta
-
-//     return `${timestamp} ${level}: ${message}`
-//   }),
-//   winston.format.colorize({
-//     all: true,
-//   }),
-//   winston.format.align(),
-// )
-// export const logger = winston.createLogger({
-//   level: process.env.LOG_LEVEL || "info",
-//   format: logFormatter,
-//   transports: [new winston.transports.Console()],
-// })
