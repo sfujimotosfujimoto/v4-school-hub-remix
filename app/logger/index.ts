a5082e8d1ad55513bb5e7f1e5e507ddd5ba9795a
sfujimotosfujimoto <unicoshun@gmail.com>
2024-02-02 20:52:01 +0900

    update comment

--- app/logger/index.ts
+++ app/logger/index.ts
@@ -10,13 +10,6 @@ const stream = pretty({
 export const logger = pino(
   {
     level: process.env.NODE_ENV === "development" ? "debug" : "info",
-    // name: "sf-logger",
-    // transport: {
-    //   target: "pino-pretty",
-    //   options: {
-    //     colorize: true,
-    //   },
-    // },
   },
   stream,
 )
