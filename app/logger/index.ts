a26d22aa281cb74dbeb9c8d7f2d114cb73218d8a
sfujimotosfujimoto <unicoshun@gmail.com>
2024-02-02 20:37:51 +0900

    delete comment for logger

--- app/logger/index.ts
+++ app/logger/index.ts
@@ -9,25 +9,3 @@ export const logger = pino({
     },
   },
 })
-
-// import winston from "winston"
-
-// const logFormatter = winston.format.combine(
-//   winston.format.timestamp({
-//     format: "YY-MM-DD hh:mm:ss",
-//   }),
-//   winston.format.printf((meta) => {
-//     const { level, message, timestamp } = meta
-
-//     return `${timestamp} ${level}: ${message}`
-//   }),
-//   winston.format.colorize({
-//     all: true,
-//   }),
-//   winston.format.align(),
-// )
-// export const logger = winston.createLogger({
-//   level: process.env.LOG_LEVEL || "info",
-//   format: logFormatter,
-//   transports: [new winston.transports.Console()],
-// })
