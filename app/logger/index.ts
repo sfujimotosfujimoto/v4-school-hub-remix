11acd4e17476d717d91cb4cc502d6f1165ee1f33
sfujimotosfujimoto <unicoshun@gmail.com>
2024-02-02 20:35:59 +0900

    update pino-pretty to stream

--- app/logger/index.ts
+++ app/logger/index.ts
@@ -1,14 +1,25 @@
 import pino from "pino"
+import pretty from "pino-pretty"
 
-export const logger = pino({
-  level: process.env.LOG_LEVEL || "info",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-    },
-  },
+const stream = pretty({
+  levelFirst: true,
+  colorize: true,
+  ignore: "hostname,pid",
 })
+
+export const logger = pino(
+  {
+    level: process.env.NODE_ENV === "development" ? "debug" : "info",
+    // name: "sf-logger",
+    // transport: {
+    //   target: "pino-pretty",
+    //   options: {
+    //     colorize: true,
+    //   },
+    // },
+  },
+  stream,
+)
 
 // import winston from "winston"
 
